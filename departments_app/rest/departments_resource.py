467683a51352ef1bf1f95c01eb810b7b26e84eb8
Artur Shapovalov <shapovalov.artur@gmail.com>
2021-06-01 21:58:58 +0300

    refactor

--- departments_app/rest/departments_resource.py
+++ departments_app/rest/departments_resource.py
@@ -1,20 +1,15 @@
 from flask import request
 from flask_restful import Resource
-from marshmallow import ValidationError
 from . import rest_api
 from departments_app.models.department import Department
-from departments_app.service.schemas import department_schema, departments_schema
-from departments_app import db
-from sqlalchemy.exc import IntegrityError
 from departments_app.service.services import DepartmentService
 
 
 class Departments(Resource):
     def get(self, uuid: str = None):
         """
-        Process GET request on resource,
-        Returns collection of all departments if uuid is not given, and response status code 200
-        Returns one department object and status code 200 if uuid is given
+        Process GET request on resource. Returns collection of all departments if uuid is not given,
+        and response status code 200 Returns one department object and status code 200 if uuid is given
         In other cases returns response code - 404
         """
         # all
@@ -28,8 +23,7 @@ class Departments(Resource):
 
     def post(self):
         """
-        Process POST request on resource,
-        Returns status code 201 and new resource
+        Process POST request on resource. Returns status code 201 and new resource.
         """
         # Check input
         json_data = request.get_json()
@@ -43,33 +37,20 @@ class Departments(Resource):
 
     def put(self, uuid: str):
         """ Process PUT request on resource, updating it """
-
         # Check input
         json_data = request.get_json()
         if not json_data:
             return {"message": "No input data provided"}, 400
-        # Validate and deserialize input
-        # try:
-        #     data = department_schema.load(json_data, partial=("uuid",))
-        # except ValidationError as err:
-        #     return err.messages, 422
-        #
-        # # query existing record in not found - return 404
-        # db_record = Department.get_by_uuid(uuid)
-        # if not db_record:
-        #     return {}, 404
-        #
-        # # update record if there is updated field
-        # if name := data.get("name"):
-        #     db_record.name = name
-        # try:
-        #     db.session.commit()
-        # except IntegrityError:
-        #     return {}, 409
-        # return {"message": "resource updated"}, 200
-        result = DepartmentService.update_one_department(uuid=uuid, json_data=json_data)
 
+        result, message = DepartmentService.update_one_department(uuid=uuid, data=json_data)
+        if result == 'validation error':
+            return message, 422
+        if result == "not found":
+            return message, 404
+        if result == "duplicate":
+            return message, 409
 
+        return message, 200
 
     def delete(self, uuid: str):
         """
